15ee9fc5e2d2459f14636871f0c8b954a96d69af
cuyu <icyarm@qq.com>
2017-02-23 11:20:09 +0800

    Integrate with redux dev chrome  plugin.

--- src/Store.js
+++ src/Store.js
@@ -5,6 +5,10 @@ import {createStore, combineReducers} from 'redux'
 import valueReducer from './reducers/valueReducer'
 
 let reducer = combineReducers({value: valueReducer});
-let store = createStore(reducer);
+const store = createStore(
+    reducer, /* preloadedState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+// let store = createStore(reducer);
 
 export {store}
