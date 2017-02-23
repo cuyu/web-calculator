a5302d9a92a726e78b5a20328a1c1c6471f5c890
cuyu <icyarm@qq.com>
2017-02-23 16:00:35 +0800

    Add prop define for button.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,14 @@
 /**
  * Created by cuyu on 1/20/17.
  */
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import './Button.css';
 
 class Button extends Component {
+    static propTypes = {
+        onClick: PropTypes.func.isRequired,
+    };
+
     render() {
         return (
             <button className="Button" onClick={this.props.onClick}>{this.props.name}</button>
